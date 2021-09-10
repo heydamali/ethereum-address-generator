64eaae2c1f487b18e06f172988b8d5e0e3b9402c
heydamali <arinze.kingsley2012@gmail.com>
2021-09-10 18:25:15 +0100

    Initial commit

--- main.js
+++ main.js
@@ -1,10 +1,43 @@
 // Add imports here
-
-
+const BIP32 = require('bip39')
+const  hdkey = require('ethereumjs-wallet/hdkey')
+const Wallet = require('ethereumjs-wallet')
+const keccak256 = require('js-sha3').keccak256
+const EthereumTx = require('ethereumjs-tx')
 
 // Add functions here
+function generateMnemonic() {
+    return BIP32.generateMnemonic()
+}
 
+function generateSeed(mnemonic) {
+    return BIP32.mnemonicToSeed(mnemonic)
+}
 
+function generatePrivKey(mnemonic) {
+    const seed = generateSeed(mnemonic)
+    return hdkey.fromMasterSeed(seed).derivePath(`m/44'/60'/0'/0/0`).getWallet().getPrivateKey()
+}
+
+function derivePubKey(privKey) {
+    const wallet = Wallet.fromPrivateKey(privKey)
+    return wallet.getPublicKey()
+}
+
+function deriveEthAddress(pubKey) {
+    const address = keccak256(pubKey)
+    return '0x' + address.substring(address.length - 40, address.length)
+}
+
+function signTx(privKey, txData) {
+    const tx = new EthereumTx(txData)
+    tx.sign(privKey)
+    return tx
+}
+
+function getSignerAddress(signedTx) {
+    return '0x' + signedTx.getSignerAddress().toString('hex')
+}
 
 /*
 
